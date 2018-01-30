8af95aea0ee27a8699350fc00add73b33fb3f090
kajaln123 <kajal.nerkar@citiustech.com>
2018-01-30 12:06:24 +0000

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -11,12 +11,12 @@ public class Hello {
    LDClient client = new LDClient("sdk-c44d0268-5af2-430a-b0c2-52fb43d89f9a");
 
    LDUser user = new LDUser.Builder("kajal@example.com")
-                           .firstName("kajal")
+                           .firstName("Bob")
                            .lastName("Loblaw")
                            .customString("groups", singletonList("beta_testers"))
                            .build();
 
-   boolean showFeature = client.boolVariation("user-demo", user, false);
+   boolean showFeature = client.boolVariation("user-view", user, false);
 
    if (showFeature) {
     System.out.println("Showing your feature");
